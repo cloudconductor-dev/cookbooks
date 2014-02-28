17fb1323884b5fde00e5a4f22f7feb0ae8777847
jmuto <muto.junichi@tis.co.jp>
2014-02-28 14:12:54 +0900

    use official resource apache_conf, and copy service block from apache2::default recipe

--- cc-deploy/recipes/reverse_proxy.rb
+++ cc-deploy/recipes/reverse_proxy.rb
@@ -1,9 +1,26 @@
 case node['deploy']['web']['type']
 when 'apache'
-  template "proxy.conf" do
-    path "#{node['apache']['dir']}/conf.d/proxy.conf"
-    mode '0644'
-    action :create
-    not_if node['deploy']['applications'].empty?
+  apache_conf 'proxy'
+end
+
+service 'apache2' do
+  case node['platform_family']
+  when 'rhel', 'fedora', 'suse'
+    service_name 'httpd'
+    # If restarted/reloaded too quickly httpd has a habit of failing.
+    # This may happen with multiple recipes notifying apache to restart - like
+    # during the initial bootstrap.
+    restart_command '/sbin/service httpd restart && sleep 1'
+    reload_command '/sbin/service httpd reload && sleep 1'
+  when 'debian'
+    service_name 'apache2'
+    restart_command '/usr/sbin/invoke-rc.d apache2 restart && sleep 1'
+    reload_command '/usr/sbin/invoke-rc.d apache2 reload && sleep 1'
+  when 'arch'
+    service_name 'httpd'
+  when 'freebsd'
+    service_name 'apache22'
   end
+  supports [:restart, :reload, :status]
+  action :nothing
 end
